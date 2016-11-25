724e718d0e25d870995d184f61036a8d3c236900
FranLera <FranLera@>
2016-11-25 19:26:01 +0000

    cv3 launcher is compatible with kinetic

--- src/detector_cv3.py
+++ src/detector_cv3.py
@@ -67,11 +67,10 @@ class image_converter:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = self.faceCascade.detectMultiScale(
-	gray,
-	scaleFactor=1.1,
-	minNeighbors=5,
-	minSize=(30, 30),
-	flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
     )
 
     if len(faces):
